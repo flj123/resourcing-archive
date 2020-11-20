fcd34892f1adcd81d741cad504e37f9079ffa731
flj123 <farzin.lotfijam@gmail.com>
2020-11-20 16:31:48 -0500

    Update.

--- sketch.js
+++ sketch.js
@@ -7,24 +7,24 @@ let c2;
 let fs = false;
 
 function preload() {
-  // for (let i = 0; i < 6; i++) {
-  //   imgs[i] = loadImage('assets/image'+i+'.jpg');
-  //   //print('assets/image'+i+'.jpg');
-  // }
+  for (let i = 0; i < 6; i++) {
+    imgs[i] = loadImage('assets/image'+i+'.jpg');
+    //print('assets/image'+i+'.jpg');
+  }
   let urls = [
     // 'https://ia803003.us.archive.org/1/items/centricityexhibitionphotos/1988.02_LWoods_Centricity_35.jpg',
     // 'https://ia801008.us.archive.org/6/items/1984.09lwoodsfreespaceproject12/1984.09_LWoods_FreespaceProject_10.jpg',
     // 'https://ia801008.us.archive.org/6/items/1984.09lwoodsfreespaceproject12/1984.09_LWoods_FreespaceProject_3.jpg',
     // 'https://ia903003.us.archive.org/1/items/centricityexhibitionphotos/1988.02_LWoods_Centricity_34.jpg'
-    'assets/imageLarge0.jpg',
-    'assets/image0.jpg',
-    'assets/image1.jpg'
+    // 'assets/imageLarge0.jpg',
+    // 'assets/image0.jpg',
+    // 'assets/image1.jpg'
   ];
   
-  imgs = [];
-  for (url of urls){
-    imgs.push(loadImage(url));
-  }
+  // imgs = [];
+  // for (url of urls){
+  //   imgs.push(loadImage(url));
+  // }
   //imgs[0] = loadImage(url);
 }
 
