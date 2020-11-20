b441173e12201336c79bd6ca6d75399c1d2fb160
flj123 <farzin.lotfijam@gmail.com>
2020-11-20 14:14:01 -0500

    Image size test.

--- sketch.js
+++ sketch.js
@@ -4,6 +4,7 @@ let index = 0;
 let pg;
 let c1;
 let c2;
+let fs = false;
 
 function preload() {
   // for (let i = 0; i < 6; i++) {
@@ -11,8 +12,13 @@ function preload() {
   //   //print('assets/image'+i+'.jpg');
   // }
   let urls = [
-    'https://ia801008.us.archive.org/6/items/1984.09lwoodsfreespaceproject12/1984.09_LWoods_FreespaceProject_10.jpg',
-    'https://ia801008.us.archive.org/6/items/1984.09lwoodsfreespaceproject12/1984.09_LWoods_FreespaceProject_3.jpg'
+    // 'https://ia803003.us.archive.org/1/items/centricityexhibitionphotos/1988.02_LWoods_Centricity_35.jpg',
+    // 'https://ia801008.us.archive.org/6/items/1984.09lwoodsfreespaceproject12/1984.09_LWoods_FreespaceProject_10.jpg',
+    // 'https://ia801008.us.archive.org/6/items/1984.09lwoodsfreespaceproject12/1984.09_LWoods_FreespaceProject_3.jpg',
+    // 'https://ia903003.us.archive.org/1/items/centricityexhibitionphotos/1988.02_LWoods_Centricity_34.jpg'
+    'assets/imageLarge0.jpg',
+    'assets/image0.jpg',
+    'assets/image1.jpg'
   ];
   
   imgs = [];
@@ -26,13 +32,22 @@ function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
 
+function mousePressed(){
+  fs = !fs;
+  fullscreen(fs);
+}
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
   currentImg = imgs[index];
   pg = createGraphics(currentImg.width, currentImg.height);
   
   c1 = color(235, 42, 28);
+  c1 = color(0,255,0);
+  
   c2 = color(9, 217, 172);
+  c2 = color(255,0,0);
+
 }
 
 function draw() {
@@ -60,13 +75,16 @@ function draw() {
   push();
   translate(windowWidth/2, windowHeight/2);
   imageMode(CENTER);
+  // Uniform Scale
+  scale(0.5); 
+  // Dynamic Scale
   scale(sc);
   image(pg, 0, 0);
   image(pg, 0, windowHeight/2+100, 150,150);
   pop();
   
   // Refresh Image
-  if (frameCount % 10 == 0){ // 180
+  if (frameCount % 30 == 0){ // 180
     index++;
     currentImg = imgs[index%imgs.length];
     pg = createGraphics(currentImg.width, currentImg.height);
